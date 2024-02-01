8953afd2c21ed45f957cf5c92101f14135f8c6f1
Antonio Roman <e_aroman@mercadona.com>
2024-02-01 14:07:33 +0100

    first commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "storage_class" {
+  type        = string
+  description = "Storage class of bucket"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of bucket"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Name of bucket"
+}
