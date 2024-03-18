9f94a9d5eb971a6d882c48e8585ad6f92ad998e9
Antonio Roman <e_aroman@mercadona.com>
2024-03-18 17:36:20 +0100

    validation test

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
 variable "storage_class" {
   type        = string
   description = "Storage class of bucket"
+
+  validation {
+    condition     = length(var.storage_class) < 4
+    error_message = "The storage_class var value must longer than 4"
+  }
 }
 
 variable "location" {
