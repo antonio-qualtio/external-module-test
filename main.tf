861b652b86e7255002b7e228b19d3a12c1e43d86
e_aroman <e_aroman@mercadona.com>
2025-02-26 12:53:33 +0100

    feat: itg folder delete

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "test_crossplane_bucket" {
+resource "google_storage_bucket" "test_bucket" {
     name          = var.bucket_name
     location      = var.location
     storage_class = var.storage_class
